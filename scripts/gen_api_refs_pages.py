8d85882efe8edf0e3332805c1a9f278e9d786209
konstantinos <boromir674@hotmail.com>
2023-11-15 21:48:00 +0200

    style(scripts): lint code

--- scripts/gen_api_refs_pages.py
+++ scripts/gen_api_refs_pages.py
@@ -6,16 +6,16 @@ import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
 
-src = Path(__file__).parent.parent / "src"  
+src = Path(__file__).parent.parent / "src"
 
-for path in sorted(src.rglob("*.py")):  
-    module_path = path.relative_to(src).with_suffix("")  
-    doc_path = path.relative_to(src).with_suffix(".md")  
-    full_doc_path = Path("reference", doc_path)  
+for path in sorted(src.rglob("*.py")):
+    module_path = path.relative_to(src).with_suffix("")
+    doc_path = path.relative_to(src).with_suffix(".md")
+    full_doc_path = Path("reference", doc_path)
 
     parts = list(module_path.parts)
 
-    if parts[-1] == "__init__":  
+    if parts[-1] == "__init__":
         parts = parts[:-1]
         ## https://mkdocstrings.github.io/recipes/#bind-pages-to-sections-themselves
         doc_path = doc_path.with_name("index.md")
@@ -23,14 +23,13 @@ for path in sorted(src.rglob("*.py")):
         ##
     elif parts[-1] == "__main__":
         continue
-    
+
     # Progressively build the navigation object
     nav[parts] = doc_path.as_posix()
 
-    with mkdocs_gen_files.open(full_doc_path, "w") as fd:  
-        identifier = ".".join(parts)  
-        print("::: " + identifier, file=fd)  
-
+    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+        identifier = ".".join(parts)
+        print("::: " + identifier, file=fd)
 
     # ROOT
     #  -> docs
@@ -40,7 +39,6 @@ for path in sorted(src.rglob("*.py")):
     # so that it correctly sets the edit path of (for example) nst_math.py to
     # <repo_url>/blob/master/src/artificial_artwork/nst_math.py instead of
     # <repo_url>/blob/master/docs/src/artificial_artwork/nst_math.py
-
 
     # mkdocs_gen_files.set_edit_path(full_doc_path, path)
 
